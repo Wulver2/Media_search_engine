08ea90e72c3898b3ed46f818fe3422599a5fabdc
Wulver2 <ross.shayla21@gmail.com>
2026-06-21 10:49:32 -0400

    Updated README

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Media_search_engine
+
+## features to come
+* Better search 
+* connect movie api
+* countdown feature for upcoming movies, games, etc
---
+++ media-search/src/services/api.js
@@ -0,0 +1,6 @@
+// need to create dot env file to hide my keys
+const MOVIE_API_KEY = "";
+const MOVIE_URL = "";
+
+// Find an api for anime and videogames as well
+// (TV as well if movie api doesn't include them)
