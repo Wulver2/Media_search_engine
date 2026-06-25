d4ff159acc042e2b70d7ff006531fd94bbbe974e
Wulver2 <ross.shayla21@gmail.com>
2026-06-25 08:51:16 -0400

    Add, remove, and check if favorite

--- media-search/src/contexts/MediaContext.jsx
+++ media-search/src/contexts/MediaContext.jsx
@@ -19,6 +19,24 @@ export const MediaProvider = ({children}) => {
         localStorage.setItem('favorites', JSON.stringify(favorites))
     }, [favorites])
     
+    const addFavorite = (movie) => {
+        update = [...favorites, movie]
+        setFavorites(update)
+    }
+
+    const removeFavorite = (movieID) => {
+        const element = favorites[movieID]
+        if (movieID > 0) {
+            setFavorites(favorites.filter(movie =>
+                movie !== element
+            ))
+        }
+    }
+
+    const isFavorite = (movieID) => {
+        return favorites.some(movie => movie.id == movieID)
+    }
+    
     return <MovieContext.MediaProvider>
         {children}
     </MovieContext.MediaProvider>
