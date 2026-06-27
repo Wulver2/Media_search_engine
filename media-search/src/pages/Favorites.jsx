a591922b1affdc079a1a5173e882d244bfb2ab6f
Wulver2 <ross.shayla21@gmail.com>
2026-06-27 15:35:12 -0400

    Display favorites from home page onto the favorites page

--- media-search/src/pages/Favorites.jsx
+++ media-search/src/pages/Favorites.jsx
@@ -1,4 +1,24 @@
+import { useMediaContext } from "../contexts/MediaContext"
+import MediaCard from "../components/MediaCard"
+
 function Favorite() {
+    const { favorites } = useMediaContext();
+
+    if (favorites) {
+        console.log(favorites)
+        return (
+            <div className="media-grid">
+                {favorites.map((m) => (
+                    <MediaCard key={m.mal_id} mediaData={{
+                        title: m.title, releaseDate: m.releaseDate,
+                        img_url: m.img_url, id: m.id
+                    }}>
+                    </MediaCard>
+                ))}
+
+            </div>
+        )
+    }
     return (
         <div className="favorites">
             <h2>No favorites</h2>
